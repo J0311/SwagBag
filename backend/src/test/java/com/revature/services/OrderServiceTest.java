fb3837971c216aae61486402f8e8c47f1b4abcbd
Dhairya <ddixit1@haek.iit.edu>
2022-08-26 14:49:34 -0500

    OrderService Test delete and finall

--- backend/src/test/java/com/revature/services/OrderServiceTest.java
+++ backend/src/test/java/com/revature/services/OrderServiceTest.java
@@ -1,18 +1,66 @@
 package com.revature.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.revature.models.Order;
+import com.revature.models.Purchase;
+import com.revature.models.User;
+import com.revature.repositories.OrderRepository;
 
 @SpringBootTest
 public class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    private Order o1 = new Order(1,null, LocalDateTime.now(), null);
+    private Order o2 = new Order(1,null, LocalDateTime.now(), null);
+    private Order o3 = new Order(1,null, LocalDateTime.now(), null);
+    private Order o4 = new Order(1,null, LocalDateTime.now(), null);
+    ArrayList<Order> orderList = new ArrayList<Order>();
+    
+    @BeforeEach
+    public void setup(){
+        
+        orderList.add(o1);
+        orderList.add(o2);
+        orderList.add(o3);
+        orderList.add(o4);
+        Mockito.when(orderRepository.save(o1)).thenReturn(o1);
+        Mockito.when(orderRepository.findAll()).thenReturn(orderList);   
+          
+    }
+
     @Test
     void testDelete() {
 
+        Order o = orderService.save(o1);
+        orderService.delete(o1.getOrderId()); 
+        boolean actual = orderService.findById(o.getOrderId()).isPresent();
+        assertEquals(false,actual);
     }
 
     @Test
     void testFindAll() {
 
+        List<Order> expectedList = orderService.findAll();
+        assertEquals(expectedList.size(), orderList.size());
     }
 
     @Test
