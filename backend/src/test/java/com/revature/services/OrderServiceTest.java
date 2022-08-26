97c24b2f0f12dc190131a9c1d4125cd639165e49
n8paternoster <n8paternoster@gmail.com>
2022-08-26 10:55:00 -0400

    Added test classes for order service and purchase service

---
+++ backend/src/test/java/com/revature/services/OrderServiceTest.java
@@ -0,0 +1,35 @@
+package com.revature.services;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindAllByUserId() {
+
+    }
+
+    @Test
+    void testFindById() {
+
+    }
+
+    @Test
+    void testSave() {
+
+    }
+
+    @Test
+    void testSaveAll() {
+
+    }
+}
---
+++ backend/src/test/java/com/revature/services/PurchaseServiceTest.java
@@ -0,0 +1,15 @@
+package com.revature.services;
+
+import org.junit.jupiter.api.Test;
+
+public class PurchaseServiceTest {
+    @Test
+    void testSave() {
+
+    }
+
+    @Test
+    void testSaveAll() {
+
+    }
+}
