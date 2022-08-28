071ff1764443b4081f87ed0be262e1b5aebbfb7a
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-08-28 02:06:02 -0400

    Minor navbar css change

--- frontend/src/app/components/display-orders/display-orders.component.ts
+++ frontend/src/app/components/display-orders/display-orders.component.ts
@@ -5,23 +5,21 @@ import { OrderService } from 'src/app/services/order.service';
 @Component({
   selector: 'app-orders',
   templateUrl: './display-orders.component.html',
-  styleUrls: ['./display-orders.component.css']
+  styleUrls: ['./display-orders.component.css'],
 })
 export class DisplayOrdersComponent implements OnInit {
-
   allOrders: Order[] = [];
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService) {}
 
   ngOnInit(): void {
-    console.log("onInit called for OrdersComponent");
-    this.orderService.getOrders().subscribe( // change with actual user id
+    this.orderService.getOrders().subscribe(
+      // change with actual user id
       (resp) => {
         this.allOrders = resp;
         console.log(this.allOrders);
       },
       (err) => console.log(err),
-      () => console.log("Orders Retrieved")
+      () => console.log('Orders Retrieved')
     );
   }
-
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
-import {environment} from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -49,14 +49,5 @@ export class AuthService {
     return this.http.post<any>(`${this.authUrl}/register`, payload, {
       headers: environment.headers,
     });
-  }
-
-  resetPassword(form: { email: string, password: string }): Observable<any> {
-    alert(`Password Changed! Success!
-    Email: ${form.email}
-    Password: ${form.password}
-    `)
-    return of(form)
-    // return this.http.post<any>(`${this.authUrl}/reset-password`, form, {headers: environment.headers})
   }
 }
