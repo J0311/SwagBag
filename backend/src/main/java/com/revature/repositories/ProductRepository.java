bfde0dc5c6236daeb0d601bed76fa196acfe41e1
Dhairya <ddixit1@haek.iit.edu>
2022-08-29 12:35:29 -0500

    added product repo

--- backend/src/main/java/com/revature/repositories/ProductRepository.java
+++ backend/src/main/java/com/revature/repositories/ProductRepository.java
@@ -1,7 +1,17 @@
 package com.revature.repositories;
 
 import com.revature.models.Product;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query(value = "select * from product where name like concat('%',:keyword,'%')", nativeQuery = true)
+    public List<Product> getSearchedProducts(@Param ("keyword") String keyword );
+         
 }
