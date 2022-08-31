2fe279facc83cf622a98b5a64bd2bc1a5967b70f
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-08-31 02:27:16 -0400

    Added table with action buttons to display products in admin page

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  loggedInUser: any = JSON.parse(sessionStorage.getItem('loggedInUser') || '');
+  allProducts: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      (resp) => (this.allProducts = resp),
+      (err) => console.log(err),
+      () => console.log('Products Retrieved')
+    );
   }
-
 }
