1371bca9099c08a89ce2ca064e4a75071fa062c3
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-08-31 00:57:50 -0400

    Added test admin account in data.sql and modified Admin button styling

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -39,10 +39,19 @@ INSERT INTO product (id, quantity, price, description, image, name) VALUES (
     'Coat'
 );
 
-INSERT INTO users (id, email, password, first_name, last_name) VALUES (
+INSERT INTO users (id, email, password, first_name, last_name, role) VALUES (
     1,
     'testuser@gmail.com',
     'password',
     'Test',
-    'User'
+    'User',
+    'CUSTOMER'
+), 
+(
+    2,
+    'admin@swagbag.com',
+    'password',
+    'Admin',
+    'User',
+    'ADMIN'
 );
