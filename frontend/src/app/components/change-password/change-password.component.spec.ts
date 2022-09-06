f106fa854885a32223d3652e7b36812a9ba848b1
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-09-05 20:59:13 -0400

    Modified change password component test and deleted auth-service test

--- frontend/src/app/components/change-password/change-password.component.spec.ts
+++ frontend/src/app/components/change-password/change-password.component.spec.ts
@@ -1,56 +1,133 @@
-import { ComponentFixture,TestBed } from "@angular/core/testing";
-import { AuthService } from "../../services/auth.service";
-import { ChangePasswordComponent } from "./change-password.component";
-import { NavbarComponent } from '../navbar/navbar.component';
+import { of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { ChangePasswordComponent } from './change-password.component';
 
-describe('ChangePasswordComponent',()=>{
-  let component: ChangePasswordComponent;
-  let fixture: ComponentFixture<ChangePasswordComponent>;
-  
+describe('ChangePasswordComponent', () => {
+  let fixture: ChangePasswordComponent;
+  let changePasswordServiceMock: any = {
+    changePassword: jest.fn(),
+  };
+  const routerMock: Router = {
+    navigate: jest.fn(),
+  } as any;
 
-  let authServiceMock: any;
-
-  beforeEach(async()=>{
-
-    authServiceMock={
-      changePassword: jest.fn()
-    }
-    await TestBed.configureTestingModule({
-      declarations: [ChangePasswordComponent, NavbarComponent],
-
-      
-      providers:[
-        {
-          provide:AuthService, useValue:authServiceMock
-
-      }
-    ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    fixture = new ChangePasswordComponent(
+      changePasswordServiceMock,
+      routerMock
+    );
   });
 
-  beforeEach(()=>{
-    fixture=TestBed.createComponent(ChangePasswordComponent);
-    component = fixture.componentInstance;
-    //component = fixture.debugElement.componentInstance
-    fixture.detectChanges;
+  describe('Setup component', () => {
+    describe('changePasswordForm', () => {
+      it('should be defined', () => {
+        expect(fixture.changePasswordForm).toBeDefined();
+      });
+    });
+
+    describe('message', () => {
+      it('should be empty', () => {
+        expect(fixture.message).toBe('');
+      });
+    });
+
+    describe('ngOnInit', () => {
+      it('should be defined', () => {
+        expect(fixture.ngOnInit).toBeDefined();
+      });
+    });
   });
 
-  it('should create',()=>{
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('should be called', () => {
+      const ngOnInitSpy = jest.spyOn(fixture, 'ngOnInit');
+
+      fixture.ngOnInit();
+
+      expect(ngOnInitSpy).toHaveBeenCalled();
+    });
   });
 
-  // it('should render title in a h1 tag', async() => {
-  //      // const fixture = TestBed.createComponent(ChangePasswordComponent);
-  //      // fixture.detectChanges();
-  //       const compiled = fixture.debugElement.nativeElement;
-  //       expect(
-  //         compiled.querySelector('label[for=inputOldPassword]').textContent
-  //       ).toContain('Old Password');
-  //     });
+  describe('onSubmit', () => {
+    describe('when oldPassword, newPassword and confirmPassword are empty', () => {
+      it('should set message to Please fill in all fields', () => {
+        // Arrange
+        fixture.changePasswordForm.controls.oldPassword.setValue('');
+        fixture.changePasswordForm.controls.newPassword.setValue('');
+        fixture.changePasswordForm.controls.confirmPassword.setValue('');
+
+        // Act
+        fixture.onSubmit();
+
+        // Assert
+        expect(fixture.message).toBe('Please fill in all fields');
+      });
+    });
+
+    describe('when newPassword is less than 6 characters', () => {
+      it('should set message to The password is too short', () => {
+        fixture.changePasswordForm.controls.oldPassword.setValue('123456');
+        fixture.changePasswordForm.controls.newPassword.setValue('12345');
+        fixture.changePasswordForm.controls.confirmPassword.setValue('12345');
+
+        fixture.onSubmit();
+
+        expect(fixture.message).toBe('The password is too short');
+      });
+    });
+
+    describe('when newPassword does not match confirmPassword', () => {
+      it('should set message to The new password does not match confirm password', () => {
+        fixture.changePasswordForm.controls.oldPassword.setValue('123456');
+        fixture.changePasswordForm.controls.newPassword.setValue('123456');
+        fixture.changePasswordForm.controls.confirmPassword.setValue('1234567');
+
+        fixture.onSubmit();
+
+        expect(fixture.message).toBe(
+          'The new password does not match confirm password'
+        );
+      });
+    });
+
+    describe('when oldPassword, newPassword and confirmPassword are valid', () => {
+      it('should call authService.changePassword', () => {
+        fixture.changePasswordForm.controls.oldPassword.setValue('123456');
+        fixture.changePasswordForm.controls.newPassword.setValue('123456');
+        fixture.changePasswordForm.controls.confirmPassword.setValue('123456');
+
+        jest
+          .spyOn(changePasswordServiceMock, 'changePassword')
+          .mockImplementation(() => {
+            return of({
+              subscribe: jest.fn(),
+            });
+          });
+
+        fixture.onSubmit();
+
+        expect(changePasswordServiceMock.changePassword).toHaveBeenCalled();
+      });
+    });
+
+    describe('when authService.changePassword returns an error', () => {
+      it('should set message to the error', () => {
+        fixture.changePasswordForm.controls.oldPassword.setValue('123456');
+        fixture.changePasswordForm.controls.newPassword.setValue('123456');
+        fixture.changePasswordForm.controls.confirmPassword.setValue('123456');
+
+        jest
+          .spyOn(changePasswordServiceMock, 'changePassword')
+          .mockImplementation(() => {
+            return throwError({
+              error: 'test error',
+            });
+          });
+
+        fixture.onSubmit();
+
+        expect(fixture.message).toBe('test error');
+      });
+    });
+  });
 });
-
-
-
-
-
--- frontend/src/app/components/change-password/change-password.component.ts
+++ frontend/src/app/components/change-password/change-password.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ChangePasswordService } from '../../services/change-password.service';
 
 @Component({
   selector: 'app-change-password',
@@ -15,36 +15,41 @@ export class ChangePasswordComponent implements OnInit {
     confirmPassword: new FormControl(''),
   });
   message: string = '';
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: ChangePasswordService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   onSubmit(): void {
     const formValues = this.changePasswordForm.value;
     if (
-      formValues.oldPassword.length == 0 ||
-      formValues.newPassword == 0 ||
-      formValues.confirmPassword == 0
+      formValues.oldPassword.length === 0 ||
+      formValues.newPassword.length === 0 ||
+      formValues.confirmPassword.length === 0
     ) {
       this.message = 'Please fill in all fields';
       return;
     }
+
     if (formValues.newPassword.length < 6) {
       this.message = 'The password is too short';
       return;
     }
+
     if (formValues.newPassword !== formValues.confirmPassword) {
       this.message = 'The new password does not match confirm password';
       return;
     }
 
     this.authService.changePassword(formValues).subscribe(
-      (resp) => console.log('Password changed successfully!'),
-      (err) => (this.message = err.error),
       () => {
+        console.log('Password changed successfully!');
         sessionStorage.clear();
         this.router.navigate(['/login']);
-      }
+      },
+      (err) => (this.message = err.error)
     );
   }
 }
